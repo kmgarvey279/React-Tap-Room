48f0eb7aac99c05c9a4bb1deb76e97a7ecf7a617
Kevin Garvey <kmgarvey279@gmail.com>
2019-07-12 09:01:39 -0700

    add content to employees component

---
+++ src/components/Employees.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import KegList from './KegList';
+import EditKegForm from './EditKegForm';
+import NewKegForm from './NewKegForm';
+
+function Employees(props){
+  let optionalEditKegComponent = null;
+  if (props.selectedKeg !=null){
+    optionalEditKegComponent = <EditKegForm selectedKeg={props.kegList[props.selectedKeg]}/>;
+  }
+  return (
+    <div>
+      <h2>Employee Functions</h2>
+      {optionalEditKegComponent}
+      <KegList
+        kegList={props.kegList}
+        currentRouterPath={props.currentRouterPath}
+        onKegSelection={props.onKegSelection}/>
+      <NewKegForm/>
+    </div>
+  );
+}
+  
+Employees.propTypes = {
+  kegList: PropTypes.object,
+  currentRounterPath: PropTypes.string.isRequired,
+  onKegSelection: PropTypes.func.isRequired,
+  selectedKeg: PropTypes.string
+};
+
+export default Employees;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,7 +32,7 @@ function Header(){
       <div id="title"><h1>Tap Room</h1></div>
       <img id="background" src={background}/>
       <div id="navbar">
-        <Link to="/">Home</Link> | <Link to="/keglist">Kegs on Tap</Link> | <Link to="/newkegform">Add New Keg</Link> | <Link to="/aboutus">About Us</Link> 
+        <Link to="/">Home</Link> | <Link to="/keglist">Kegs on Tap</Link> | <Link to="/aboutus">About Us</Link> 
       </div>
     </div>
   );
